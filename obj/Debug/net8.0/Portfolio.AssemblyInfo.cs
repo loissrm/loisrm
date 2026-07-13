7c7aa50dde7fce71c2c7f6a09e13a388dcd9b91a
lois rodríguez <loiss.rodriguezz@gmail.com>
2026-07-13 21:05:19 +0200

    Solucion pequeños fallos de traducción y añadir foto de inicio

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc6a423846c508828455f1c7d747171bee110ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa00a7764565dcbef3fd5479cba13fe20c621269")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
