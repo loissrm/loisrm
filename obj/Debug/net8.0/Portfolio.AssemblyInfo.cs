20732fd0902fc1f0fd33ff29f3ebf06c3fc83c43
lois rodríguez <loiss.rodriguezz@gmail.com>
2026-07-15 12:06:46 +0200

    Actualizacon página AudiHub, cambio colores, pagina Porximamente

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7aa50dde7fce71c2c7f6a09e13a388dcd9b91a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad2326af3ae2dc7b826860ccaf4c97d8fd4dd6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
