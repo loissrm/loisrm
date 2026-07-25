82eaf9ce80aba117d449e223a25df197c053bce8
lois rodríguez <loiss.rodriguezz@gmail.com>
2026-07-25 18:12:02 +0200

    Archivo verificacion google

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77784a75ed9b8e7d25338308888b650a0a05819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1668b75fcf6f2162481c523172bb250944e8c72c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
