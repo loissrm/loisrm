9310a0c0999b6e6313a88dcdd1593165914cdcc2
lois rodríguez <loiss.rodriguezz@gmail.com>
2026-08-22 01:45:04 +0200

    feat(projects): add 'Frog Jumping' project page
    Include full page layout, custom styles, and multi-language support.

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5500986db09f0a3059179f0b5d949904a7bb7a08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87a8287b74054855bcfacc31cda0c774caa8935")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
