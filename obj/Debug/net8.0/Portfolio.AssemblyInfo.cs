60d95463f94a472b9b1fb8cd7633bbaec8686095
lois rodríguez <loiss.rodriguezz@gmail.com>
2026-09-03 15:01:12 +0200

    correccion enlace head meta

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9310a0c0999b6e6313a88dcdd1593165914cdcc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+363890c234e0048d61658c5fc0007a38385b5f5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
